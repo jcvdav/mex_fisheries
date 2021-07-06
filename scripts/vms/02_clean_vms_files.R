95c83d25246db16d143923d9234abddfe567dc11
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2021-07-06 13:59:26 -0700

    Updating exporting scheme

--- scripts/vms/02_clean_vms_files.R
+++ scripts/vms/02_clean_vms_files.R
@@ -25,34 +25,39 @@ to_datetime <- function(x) {
   return(datetime)
 }
 
-fix_rnpa <- function(rnpa){
-  lengths <- str_length(rnpa)
-  missing <- pmax(8 - lengths, 0)
-  zeroes <- map_chr(missing, ~paste(numeric(length = .x), collapse = ""))
-  out <- paste0(zeroes, rnpa)
-  return(out)
-}
-
 clean_vms <- function(data, year, month) {
   dt <- data %>% 
     pull(path) %>% 
     map_dfr(fread,
             select = 1:9,
-            col.names = c("name", "rnpa", "port", "economic_unit", "datetime", "lat", "lon", "speed", "course"),
+            colClasses = "character",
+            col.names = c("name", "vessel_rnpa", "port", "economic_unit", "datetime", "lat", "lon", "speed", "course"),
             na.strings = c("NULL", "NA"),
             blank.lines.skip = TRUE)
   
   dt[, `:=` (datetime = to_datetime(datetime), lat = as.numeric(lat), lon = as.numeric(lon))]
-  dt[, rnpa := fix_rnpa(rnpa)]
+  dt[, vessel_rnpa := fix_rnpa(vessel_rnpa)]
   dt$year <- year
   dt$month <- as.numeric(month)
   
+  # vessel_name_dictionary <- tibble(name = unique(dt$name)) %>% 
+    # mutate(vessel_name_norm = map_chr(name, normalize_shipname)) %>% 
+    # as.data.table()
+  
+  # setkey(vessel_name_dictionary, "name")
+  # setkey(dt, "name")
+
+  # dt <- merge(dt, vessel_name_dictionary, by = "name")
+  # dt[name = NULL]
+  
+  # return(dt)
+  
   fwrite(x = dt,
-         file = file.path(project_path, "processed_data", paste0("MXN_VMS_", year, "_", month, ".csv")))
+         file = file.path(project_path, "processed_data", "MEX_VMS", paste0("MEX_VMS_", year, "_", month, ".csv")))
 }
 
 
-paths <- list.files(path = file.path(project_path, "raw_data", "VMS"),
+paths <- list.files(path = file.path(project_path, "raw_data", "MEX_VMS"),
                     recursive = T,
                     pattern = "*.csv",
                     full.names = T)
@@ -80,11 +85,11 @@ metadata <- tibble(path = paths) %>%
          year_month = paste(year, month, sep = "_"))
 
 # READ ALL
-plan(multisession, workers = 14)
+plan(multisession, workers = 5)
 
 metadata %>% 
   select(year, month, path) %>%
-  nest(data = path) %$% 
+  nest(data = path) %$%
   future_pwalk(.l = list(data = data, year = year, month = month), clean_vms)
 
 plan(sequential)
