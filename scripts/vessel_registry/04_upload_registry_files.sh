ce5cc87f563fe6a42e4e94586ec8a2e7164e6a21
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2021-07-06 14:20:02 -0700

    New script to upload vessel registry

---
+++ scripts/vessel_registry/04_upload_registry_files.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Upload vessel registry
+gsutil cp complete_vessel_registry.csv gs://mex_fisheries/
+
+bq rm -f -t emlab-gcp:mex_fisheries.vessel_info
+bq mk --table \
+--schema eu_rnpa:STRING,economic_unit:STRING,vessel_rnpa:STRING,vessel_name:STRING,owner_rnpa:STRING,owner_name:STRING,hull_identifier:STRING,target_species:STRING,home_port:STRING,construction_year:INTEGER,hull_material:STRING,preservation_system:STRING,gear_type:STRING,detection_gear:STRING,vessel_type:STRING,vessel_length_m:NUMERIC,vessel_beam_m:NUMERIC,vessel_height_m:NUMERIC,vessel_draft_m:NUMERIC,vessel_gross_tonnage:NUMERIC,captain_num:INTEGER,engineer_num:INTEGER,s_fisher_num:INTEGER,fisher_num:INTEGER,sfc_gr_kwh:NUMERIC,engine_power_hp:NUMERIC,engine_power_bin_hp:NUMERIC,design_speed_kt:FLOAT \
+--description "Vessel registry" \
+emlab-gcp:mex_fisheries.vessel_info2
+
+bq load \
+--source_format=CSV \
+--skip_leading_rows=1 \
+--replace \
+emlab-gcp:mex_fisheries.vessel_info \
+gs://mex_fisheries/complete_vessel_registry.csv
