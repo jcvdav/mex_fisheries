736ab7427f6fa9758f3bc0dc8ca33afd26c0e7fb
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2021-07-16 12:44:25 -0700

    Moved function to set up folder

--- scripts/vessel_registry/01_large_scale_vessel_registry.R
+++ scripts/vessel_registry/01_large_scale_vessel_registry.R
@@ -12,21 +12,6 @@ library(readxl)
 library(furrr)
 library(tidyverse)
 
-## Define functions we'll use
-# String-fixing function
-str_fix <- function(x) {
-  x <- str_to_upper(x)        # String to upper
-  x <-
-    str_trim(x)            # Trim leading and trailing whites paces
-  x <- str_squish(x)          # Squish repeated white spaces
-  return(x)                   # Return clean string
-}
-
-# Design speed function
-design_speed <- function(engine_power_hp) {
-  engine_power_kwh <- (engine_power_hp / 1.34102)                           # Convert to kwh
-  10.4818 + (1.2e-3 * engine_power_kwh) - (3.84e-8 * engine_power_kwh ^ 2)  # Calculate design speed
-}
 
 ## Load data  ###############################################################################################################################################
 # Maximum daily liters for each engine size and fuel type
