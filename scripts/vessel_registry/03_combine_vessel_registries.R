0ddc40c6f1a77e1023923fb21c767983e1fbd22d
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2021-07-16 12:45:45 -0700

    Added log recording

--- scripts/vessel_registry/03_combine_vessel_registries.R
+++ scripts/vessel_registry/03_combine_vessel_registries.R
@@ -1,13 +1,17 @@
 
+library(data.table)
+library(tidyverse)
 
-files <- list.files(path = file.path(project_path, "processed_data", "MEX_VESSEL_REGISTRY"), pattern = "csv", full.names = T)
+
+files <- list.files(path = file.path(project_path, "processed_data", "MEX_VESSEL_REGISTRY"), pattern = "scale", full.names = T)
 
 vessel_registry <- map_dfr(files,
                            fread) %>% 
-  select(-c(brand, model)) %>% 
   mutate(vessel_rnpa = fix_rnpa(vessel_rnpa)) %>%
   distinct()
 
 fwrite(vessel_registry,
        file.path(project_path, "processed_data", "MEX_VESSEL_REGISTRY", "complete_vessel_registry.csv"),
        append = F)
+
+system("date >> scripts/vessel_registry/registry.log")
