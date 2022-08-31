b1bf1c62f895f9b2e6bc89aa6ee586b03183bf67
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2022-08-31 16:22:13 -0700

    updating log

--- scripts/vms/03_upload_vms_files.sh
+++ scripts/vms/03_upload_vms_files.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
+# Set path
+export PROJECT_PATH="/Volumes/GoogleDrive/Shared drives/emlab/projects/current-projects/mex-fisheries"
+
 # Upload all local csv files to GCS Bucket
 gsutil cp "$PROJECT_PATH"/processed_data/MEX_VMS/*.csv gs://mex_fisheries/MEX_VMS
 
 # Create a partitioned table in Big Query
-bq rm -f -t emlab-gcp:mex_fisheries.mex_vms
+#bq rm -f -t emlab-gcp:mex_fisheries.mex_vms_v_20220323
 bq mk --table \
---schema name:STRING,vessel_rnpa:STRING,port:STRING,economic_unit:STRING,datetime:DATETIME,lat:FLOAT,lon:FLOAT,speed:NUMERIC,course:INTEGER,year:INTEGER,month:INTEGER \
+--schema src:STRING,name:STRING,vessel_rnpa:STRING,port:STRING,economic_unit:STRING,datetime:DATETIME,lat:FLOAT,lon:FLOAT,speed:NUMERIC,course:INTEGER,year:INTEGER,month:INTEGER \
 --time_partitioning_field datetime \
 --time_partitioning_type YEAR \
---description "Mexican VMS data, with some caveats" \
-emlab-gcp:mex_fisheries.mex_vms
+--description "Mexican VMS data" \
+emlab-gcp:mex_fisheries.mex_vms_v_20220323
 
 # Upload from GCS bcket to Big Query table
 bq load \
 --source_format=CSV \
 --skip_leading_rows=1 \
 --replace \
-emlab-gcp:mex_fisheries.mex_vms \
-gs://mex_fisheries/MEX_VMS/MXN_VMS*.csv
+emlab-gcp:mex_fisheries.mex_vms_v_20220323 \
+gs://mex_fisheries/MEX_VMS/MEX_VMS*.csv
 
 # Save breadcrumb
 date >> scripts/vms/upload.log
