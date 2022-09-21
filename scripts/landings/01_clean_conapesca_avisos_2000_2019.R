99236d66c8093e45c07cbceac3b56840cd6637ff
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2022-09-20 17:23:49 -0700

    Updated landings v1

--- scripts/landings/01_clean_conapesca_avisos_2000_2019.R
+++ scripts/landings/01_clean_conapesca_avisos_2000_2019.R
@@ -1,7 +1,10 @@
 # Load packages
+library(here)
 library(janitor)
 library(data.table)
 library(tidyverse)
+
+source(here("scripts", "00_setup.R"))
 
 # Function to read and clean
 my_read <- function(path){
@@ -34,7 +37,7 @@ my_read <- function(path){
            value = as.numeric(value))
 }
 
-files <- list.files(path = file.path(data_sets, "mex_fisheries", "mex_landings", "CONAPESCA Avisos 2000-2019"),
+files <- list.files(path = here("data", "mex_landings", "raw", "CONAPESCA Avisos 2000-2019"),
                     pattern = "*.csv",
                     full.names = T)
 
@@ -42,7 +45,7 @@ dt <- map_dfr(files, my_read)
 
 setkey(dt, eu_rnpa, vessel_rnpa, year_cut)
 
-landings <- dt %>% 
+annual_vessel_landings <- dt %>% 
   .[!acuaculture_production == "SÍ"| 
       !fishing_zone_type == "AGUAS CONTINENTALES" |
       !is.na(eu_rnpa) |
@@ -53,9 +56,52 @@ landings <- dt %>%
         value = sum(value, na.rm = T)),
     by = .(year_cut, eu_rnpa, vessel_rnpa, main_species_group)] 
 
+monthly_vessel_landings <- dt %>% 
+  .[!acuaculture_production == "SÍ"| 
+      !fishing_zone_type == "AGUAS CONTINENTALES" |
+      !is.na(eu_rnpa) |
+      !is.na(vessel_rnpa)] %>% 
+  .[,`:=`(eu_rnpa = fix_rnpa(rnpa = eu_rnpa, length = 10),
+          vessel_rnpa = fix_rnpa(rnpa = vessel_rnpa))] %>%
+  .[, .(landed_weight = sum(landed_weight, na.rm = T),
+        value = sum(value, na.rm = T)),
+    by = .(year_cut, month_cut, eu_rnpa, vessel_rnpa, main_species_group)] 
 
-saveRDS(object = landings,
-        file = file.path(data_sets, "mex_fisheries", "mex_landings", "clean", "mex_annual_landings_by_vessel.rds"))
+
+## ECONOMIC UNIT
+
+annual_eu_landings <- dt %>% 
+  .[!acuaculture_production == "SÍ"| 
+      !fishing_zone_type == "AGUAS CONTINENTALES" |
+      !is.na(eu_rnpa) |
+      !is.na(vessel_rnpa)] %>% 
+  .[,`:=`(eu_rnpa = fix_rnpa(rnpa = eu_rnpa, length = 10))] %>%
+  .[, .(landed_weight = sum(landed_weight, na.rm = T),
+        value = sum(value, na.rm = T)),
+    by = .(year_cut, eu_rnpa, main_species_group)] 
+
+monthly_eu_landings <- dt %>% 
+  .[!acuaculture_production == "SÍ"| 
+      !fishing_zone_type == "AGUAS CONTINENTALES" |
+      !is.na(eu_rnpa) |
+      !is.na(vessel_rnpa)] %>% 
+  .[,`:=`(eu_rnpa = fix_rnpa(rnpa = eu_rnpa, length = 10))] %>%
+  .[, .(landed_weight = sum(landed_weight, na.rm = T),
+        value = sum(value, na.rm = T)),
+    by = .(year_cut, month_cut, eu_rnpa, main_species_group)] 
+
+
+saveRDS(object = annual_vessel_landings,
+        file = here("data", "mex_landings", "clean", "mex_annual_landings_by_vessel.rds"))
+
+saveRDS(object = monthly_vessel_landings,
+        file = here("data", "mex_landings", "clean", "mex_monthly_landings_by_vessel.rds"))
+
+saveRDS(object = annual_eu_landings,
+        file = here("data", "mex_landings", "clean", "mex_annual_landings_by_eu.rds"))
+
+saveRDS(object = monthly_eu_landings,
+        file = here("data", "mex_landings", "clean", "mex_monthly_landings_by_eu.rds"))
 
 
 
